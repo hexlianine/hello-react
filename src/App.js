d88502084b26622b6551f1d703484be79107db04
hexlianine <1669264+hexlianine@users.noreply.github.com>
2026-05-17 21:01:53 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+export default function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <div>{/* status */}</div>
+        <ol>{/* TODO */}</ol>
+      </div>
+    </div>
+  );
+}
+
+function Board() {
+  return (
+    <div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </div>
+  );
+}
+
+function Square() {
+  return <button className="square">X</button>;
+}
